862191df73264a689d511f2b37c6fcac67f38831
Yukikaze! <haoda_li@berkeley.edu>
2023-07-24 11:03:35 -0400

    bump the version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="mkdocs-plotly-plugin",
-    version="0.1.2",
+    version="0.1.3",
     description="MkDocs plugin to add plotly charts from plotly's json data",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -19,10 +19,6 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License",
     ],
     install_requires=["mkdocs>=1.1", "pymdown-extensions>=9.2", "beautifulsoup4>=4.11.1"],
